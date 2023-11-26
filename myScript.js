43f35606a25e69fb4884e10fb8691b7cf39ccee1
Fada-dev <anthonyaffulkwabenabroni@gmail.com>
2023-11-26 07:40:55 -1200

    complete undergraduate page and some little changes

--- myScript.js
+++ myScript.js
@@ -6,4 +6,16 @@ function hideMenu() {
     navLinks.style.right = "-200px";
 }
 
-
+/*---For Courses Yearly Or Semester */
+document.addEventListener('DOMContentLoaded', function() {
+    const expandingDivs = document.querySelectorAll('.level');
+  
+    expandingDivs.forEach(function(div) {
+      const expandingContent = div.querySelector('.expanding-content');
+  
+      div.addEventListener('click', function() {
+        this.classList.toggle('expanded');
+        expandingContent.style.display = 'block';
+      });
+    });
+  });
